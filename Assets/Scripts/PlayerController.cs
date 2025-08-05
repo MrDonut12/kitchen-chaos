0cb93c385ef6106de569d680a9535508045589e8
Mr.Donut <KhanhNDCE200196@gmail.com>
2025-08-05 15:43:36 +0700

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    [SerializeField] private float speed = 5.4f;
+
+    [SerializeField] private float move_speed = 5.4f;
+    [SerializeField] private float rotate_speed = 10.4f;
     private void Update()
     {
         Vector3 InputVector = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W))
         {
-            InputVector.z = +1;
+            InputVector.z = -1;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
 
-            InputVector.x = -1;
+            InputVector.x = +1;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            InputVector.z = -1;
+            InputVector.z = +1;
         }
         
         if (Input.GetKey(KeyCode.D))
         {
-            InputVector.x = +1;
+            InputVector.x = -1;
         }
 
         InputVector = InputVector.normalized;
-        transform.position += InputVector * Time.deltaTime * speed;
+        transform.position += InputVector * Time.deltaTime * move_speed;
+        // transform.forward = InputVector;
+        transform.forward = Vector3.Lerp(transform.forward, (InputVector), Time.deltaTime * rotate_speed);
         Debug.Log(InputVector);
+
     }
 }
