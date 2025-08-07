236d4a9dc15385fdfa9b653257d4b08ffe8d7357
Mr.Donut <KhanhNDCE200196@gmail.com>
2025-08-07 13:27:06 +0700

    update animation that associate with player controller, transition between walking and idle animation

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private string IS_WALKING = "IsWalking";
+    [SerializeField] private PlayerController Player;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, Player.IsWalking());
+    }
+}
