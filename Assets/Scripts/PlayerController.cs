a723969924734a09e85f74438e79b283fd866d90
Mr.Donut <KhanhNDCE200196@gmail.com>
2025-08-08 18:22:25 +0700

    add tag player to player-game-object, add raycast to check _isGrounded_state, add jump control as well as modifier the specification of the playercontroller scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,60 @@
 using System;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private bool isWalking;
     [SerializeField] private float move_speed = 5.4f;
-    [SerializeField] private float rotate_speed = 10.4f;
+    [SerializeField] private float rotate_speed = 24f;
+    [SerializeField] private float jumpForce = 3f;
+    private bool isJump = false;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
+        // init
+        isJump = false;
+
+
         Vector3 InputVector = new Vector3(0, 0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            InputVector.z = 1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            InputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            InputVector.z = -1;
-        }
         
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
         {
-            InputVector.x = 1;
+            isJump = true;
         }
+        if (Input.GetKey(KeyCode.W)) InputVector.z = 1;
+        if (Input.GetKey(KeyCode.A)) InputVector.x = -1;
+        if (Input.GetKey(KeyCode.S)) InputVector.z = -1;
+        if (Input.GetKey(KeyCode.D)) InputVector.x = 1;
 
-        InputVector = InputVector.normalized;
-        transform.position += InputVector * Time.deltaTime * move_speed;
-        // transform.forward = InputVector;
+
+
+        InputVector = InputVector.normalized; 
+        rb.MovePosition(rb.position + InputVector * Time.deltaTime * move_speed);
+
         isWalking = (InputVector != Vector3.zero);
-        
-        transform.forward = Vector3.Lerp(transform.forward, (InputVector), Time.deltaTime * rotate_speed);
-        Debug.Log(InputVector);
 
+        if (isWalking) transform.forward = Vector3.Lerp(transform.forward, (InputVector), Time.deltaTime * rotate_speed);
+        
+        if (isJump && IsGrounded())
+        {
+            // transform.forward = InputVector;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+
+        // Debug line
+        Debug.DrawRay(transform.position, Vector3.down * .3f, Color.red) ;
+        if (IsGrounded())
+            Debug.Log("IS GROUNDED");
     }
 
     public bool IsWalking() { return isWalking; }
+    private bool IsGrounded() { return Physics.Raycast(transform.position, Vector3.down, .3f); }
 }
