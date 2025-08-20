e8de31814133c2a0b362e34fb6f0a91a34238f55
Mr.Donut <KhanhNDCE200196@gmail.com>
2025-08-20 16:41:13 +0700

    setup prototype for ClearCounter script, change the prefab of ClearCOunter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,31 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    private Transform SelectedBox;
+    private Transform KitchenBox;
+    [SerializeField] private bool isInteract;
+    private void Awake()
+    {
+        SelectedBox = transform.Find("SelectedBox");
+        KitchenBox = SelectedBox.Find("KitchenCounter");
+    }
+
+
     public void Interact()
     {
-        Debug.Log("Interact with CleaR COUNTER");
+        isInteract = true;
+        KitchenBox.GetComponent<MeshRenderer>().enabled = isInteract;
+    }
+
+
+    public void Pickup()
+    {
+
+    }
+    
+    public bool IsInteract()
+    {
+        return isInteract;
     }
 }
